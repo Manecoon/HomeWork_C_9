2b2866149eee6fa7b748ebb575e713ad95ccebf0
Manecoon <stas_on.85@mail.ru>
2023-01-20 00:57:16 +0300

    tasks

---
+++ task64/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача 64: Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+// N = 5 -> "5, 4, 3, 2, 1"
+// N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+
+Console.Write("Введите первое число отсчета до 1: ");
+int k = Convert.ToInt32(Console.ReadLine());
+
+void RowOfNumbers(int n)
+{
+    Console.Write($"{n} ");
+    if (n == 1) return;
+    RowOfNumbers(n - 1);
+}
+
+RowOfNumbers(k);
---
+++ task66/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+Console.Write("Введите первое число диапазона : ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите последнее число диапазона : ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int Sum(int m, int n)
+{
+    if (n == m) return m;
+    return n + Sum(m, n - 1);
+}
+Console.WriteLine($"Сумма чисел в диапазоне: {Sum(m, n)}");
